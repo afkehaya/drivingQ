78a2cd9e337f502f6cbc4671eacd49931f834c61
Alexander Kehaya <afkehaya@gmail.com>
2016-03-12 14:49:29 -0800

    updated nav added answers model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160312214234) do
+ActiveRecord::Schema.define(version: 20160312223540) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string   "answer"
+    t.integer  "question_id"
+    t.boolean  "is_complete"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "question"
